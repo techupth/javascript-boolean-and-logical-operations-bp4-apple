1edf29692c44632f1810446451de2073dac6ec67
Apple <Sasita_ssr@kkumail>
2024-05-28 23:03:09 +0700

    feat: ex 1-3

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = Boolean(1);
+let hasCriminalBlacklist = Boolean(1);
 
-let isAllow;
+let isAllow = Boolean(1);
 
 // Start coding here
+let James = isOver18 && !hasCriminalBlacklist
+isAllow = isOver18 && !hasCriminalBlacklist
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,15 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = Boolean(1);
+let isWeekday = Boolean(1);
+let hasBoughtProductFromITCategory = Boolean(1);
+let hasAttendedDiscountEvent = Boolean(1);
+let isPlatinum = Boolean(1);
 
-let hasPromotion;
+let hasPromotion = Boolean(1);
+
+let John = lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && hasAttendedDiscountEvent && !isPlatinum;
+hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent || isPlatinum);
 
 console.log(hasPromotion);
